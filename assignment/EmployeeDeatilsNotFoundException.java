7ef2ad03640c22c15411d5fb735aaf4c03ccd0b1
uplift-technoelevate <95463225+uplift-technoelevate@users.noreply.github.com>
2022-02-23 16:26:44 +0530

    directly calling super class constructor rather than overriding the toString() in sub class

--- assignment/EmployeeDeatilsNotFoundException.java
+++ assignment/EmployeeDeatilsNotFoundException.java
@@ -1,18 +1,12 @@
 package com.te.hibernatedemo.assignment;
 
 public class EmployeeDeatilsNotFoundException extends RuntimeException {
-	String msg;
 
 	public EmployeeDeatilsNotFoundException(String msg) {
-		super();
-		this.msg = msg;
+	      super(msg);
 	}
 
-	@Override
-	public String getMessage() {
 	
-		return this.msg;
-	}
 	
 	
 
